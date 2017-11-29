3483a8bf731579413b78c41fadb83cb6069d2075
Ning Zhang <yaseru2003@gmail.com>
2017-11-28 22:43:18 -0500

    Update LSP.ts

--- Link_State_Routing/LSP.ts
+++ Link_State_Routing/LSP.ts
@@ -1,12 +1,16 @@
-class LSP {
-    id: number;
+export class LSP {
+    id: string;
     sequence_number: number; //4324 or 342423532 or 1, number?
     time_to_live: number;
-    information: string[]; //TODO list of directly connected routers, or list of network names, and link costs
+    references: {[key:string]:number}; //TODO list of directly connected routers, or list of network names, and link costs
     
     //TODO: initialize variables
-    constructor(){
-
+    constructor(id: string, sequence_number: number, information: {[key:string]:number}){
+        this.id = id;
+        this.sequence_number = sequence_number;
+        this.time_to_live = 10;
+        //TODO: complete the information
+        this.references = information;
     }
     //TODO: read in information.
     read_information(){
@@ -20,7 +24,8 @@ class LSP {
     //TODO: if already have a packet, then next time just update all information for next packet.
     //sequence_number, time_to_live, information;
     update_packet(){
-        
+        this.sequence_number += 1;
+        this.time_to_live = 10;
     }
 
 }
