cca3812fb0abf7ffd4deffdfa82c78f144609eb7
Ning Zhang <yaseru2003@gmail.com>
2017-12-05 22:39:20 -0500

    Update LSP.ts
    LSP creates a link state packet, carrying along originator router's information to other routers.

--- Link_State_Routing/LSP.ts
+++ Link_State_Routing/LSP.ts
@@ -1,31 +1,20 @@
 export class LSP {
-    id: string;
+    source_router_id: string;
+    network_name: string;
     sequence_number: number; //4324 or 342423532 or 1, number?
     time_to_live: number;
-    references: {[key:string]:number}; //TODO list of directly connected routers, or list of network names, and link costs
+    references: {[key:string]:Array<any>}; //TODO list of directly connected routers, or list of network names, and link costs
     
     //TODO: initialize variables
-    constructor(id: string, sequence_number: number, information: {[key:string]:number}){
-        this.id = id;
+    constructor(id: string, network_name: string, sequence_number: number, information: {[key:string]:Array<any>}){
+        this.source_router_id = id;
+        this.network_name = network_name;
         this.sequence_number = sequence_number;
         this.time_to_live = 10;
         //TODO: complete the information
         this.references = information;
     }
-    //TODO: read in information.
-    read_information(){
-
+    decrement_ttl() {
+        this.time_to_live -= 1;
     }
-
-    //TODO: create a packet
-    create_packet(){
-
-    }
-    //TODO: if already have a packet, then next time just update all information for next packet.
-    //sequence_number, time_to_live, information;
-    update_packet(){
-        this.sequence_number += 1;
-        this.time_to_live = 10;
-    }
-
 }
