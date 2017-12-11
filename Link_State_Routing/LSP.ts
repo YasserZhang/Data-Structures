00978b8d21e862aa00ead860a80530e059a414d0
Ning Zhang <yaseru2003@gmail.com>
2017-12-11 11:08:11 -0500

    final version

--- Link_State_Routing/LSP.ts
+++ Link_State_Routing/LSP.ts
@@ -4,8 +4,7 @@ export class LSP {
     network_name: string; // network behind the router
     sequence_number: number; // the packet's sequence number //4324 or 342423532 or 1, number? A: accumulative positive integers
     time_to_live: number; // TTL: initialized at 10, counting down to 0
-    references: {[key:string]:Array<any>}; //list of directly connected routers, format => id: [network, cost, resumed_times]
-    
+    references: {[key:string]:Array<any>}; //list of directly connected routers, format => id: [network, cost]
     //initialize variables
     constructor(id: string, network_name: string, sequence_number: number, information: {[key:string]:Array<any>}){
         this.source_router_id = id;
@@ -13,9 +12,9 @@ export class LSP {
         this.sequence_number = sequence_number; // accumulative positive integers
         this.time_to_live = 10; // TTL: initialized at 10, counting down to 0
         //complete the information
-        this.references = information; // id: [network, cost, resumed_times]
+        this.references = information; // id: [network, cost]
     }
-    //cout down by 1
+    //decrease by 1
     decrement_ttl() {
         this.time_to_live -= 1;
     }
